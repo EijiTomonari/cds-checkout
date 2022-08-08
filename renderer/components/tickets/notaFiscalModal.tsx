f59d28b5fe99cf971c8f1135daa5582aa5dd84de
Eiji Tomonari <gabriel@tomonari.dev>
2022-08-08 15:27:11 -0300

    Add change modal and NF automation

---
+++ renderer/components/tickets/notaFiscalModal.tsx
@@ -0,0 +1,130 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Input,
+  ModalFooter,
+  Button,
+  Text,
+  InputGroup,
+  InputLeftAddon,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
+import { useAtom } from "jotai";
+import { useRef, useState } from "react";
+import { itemsAtom } from "../../pages/tickets";
+
+const NotaFiscalModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const [items, setitems] = useAtom(itemsAtom);
+  const [amount, setamount] = useState(0);
+  const initialRef = useRef(null);
+
+  const sendMealsOnly = () => {
+    const value =
+      Math.round(
+        items
+          .filter((item) => item.name == "Refeição")
+          .reduce((acc, item) => acc + item.price, 0) * 100
+      ) / 100;
+    fetch("api/nf/emmit?value=" + value).then(() => onClose());
+  };
+
+  const sendAll = () => {
+    const value =
+      Math.round(items.reduce((acc, item) => acc + item.price, 0) * 100) / 100;
+    fetch("api/nf/emmit?value=" + value).then(() => onClose());
+  };
+
+  const sendAmount = () => {
+    fetch("api/nf/emmit?value=" + amount).then(() => onClose());
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      initialFocusRef={initialRef}
+      size={"xl"}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textAlign={"center"}>Emitir Nota Fiscal</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody textAlign={"center"}>
+          <Text>
+            {"Número de refeições na comanda: " +
+              items.filter((item) => item.name == "Refeição").length}
+          </Text>
+          <Text>
+            {"Valor das refeições na comanda: R$ " +
+              Math.round(
+                items
+                  .filter((item) => item.name == "Refeição")
+                  .reduce((acc, item) => acc + item.price, 0) * 100
+              ) /
+                100}
+          </Text>
+          <Flex flexDir={"row"} alignItems={"center"} mt={5}>
+            <Button
+              ref={initialRef}
+              colorScheme="green"
+              type="submit"
+              form="change-form"
+              mx={2}
+              onClick={() => {
+                sendMealsOnly();
+              }}
+            >
+              Emitir só refeições
+            </Button>
+            <Button
+              colorScheme="green"
+              type="submit"
+              form="change-form"
+              mx={2}
+              onClick={() => sendAll()}
+            >
+              Emitir tudo
+            </Button>
+            <Stack mx={2}>
+              <Text>Inserir valor:</Text>
+              <InputGroup>
+                <InputLeftAddon children="R$" />
+                <Input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setamount(parseFloat(e.target.value))}
+                />
+              </InputGroup>
+              <Button
+                colorScheme="green"
+                form="change-form"
+                onClick={() => sendAmount()}
+              >
+                Emitir valor
+              </Button>
+            </Stack>
+          </Flex>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="red" mr={3} onClick={onClose}>
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default NotaFiscalModal;
--- renderer/pages/tickets.tsx
+++ renderer/pages/tickets.tsx
@@ -37,6 +37,8 @@ import useSound from "use-sound";
 import { atom, useAtom } from "jotai";
 import NewProductModal from "../components/tickets/newProductModal";
 import DiscountModal from "../components/tickets/discountModal";
+import ChangeModal from "../components/tickets/changeModal";
+import NotaFiscalModal from "../components/tickets/notaFiscalModal";
 export const itemsAtom = atom<TicketItem[]>([]);
 const Tickets: NextPage = () => {
   const [productsList, setProductsList] = useState<DocumentData[]>();
@@ -44,6 +46,7 @@ const Tickets: NextPage = () => {
   const [ticketModel, setTicketModel] = useState<DocumentData>();
   const [items, setitems] = useAtom(itemsAtom);
   const [total, settotal] = useState(0);
+  const [amountPayed, setamountPayed] = useState(0);
   const [play] = useSound("sounds/beep.wav");
   const bottomRef = useRef(null);
   const {
@@ -55,6 +58,16 @@ const Tickets: NextPage = () => {
     isOpen: isDiscountModalOpen,
     onOpen: onOpenDiscountModal,
     onClose: onCloseDiscountModal,
+  } = useDisclosure();
+  const {
+    isOpen: isChangeModalOpen,
+    onOpen: onOpenChangeModal,
+    onClose: onCloseChangeModal,
+  } = useDisclosure();
+  const {
+    isOpen: isNotaFiscalModalOpen,
+    onOpen: onOpenNotaFiscalModal,
+    onClose: onCloseNotaFiscalModal,
   } = useDisclosure();
   useEffect(() => {
     fetchProductsList().then((data) => {
@@ -133,116 +146,147 @@ const Tickets: NextPage = () => {
   return (
     <Hotkeys keyName="f9" onKeyDown={removeAllItems}>
       <Hotkeys keyName="f2" onKeyDown={onOpenDiscountModal}>
-        <Flex w={"100vw"} h={"100vh"} flexDir={"row"}>
-          <NewProductModal
-            isOpen={isNewProductModalOpen}
-            onClose={onCloseNewProductModal}
-          ></NewProductModal>
-          <DiscountModal
-            isOpen={isDiscountModalOpen}
-            onClose={onCloseDiscountModal}
-            total={total}
-            setItems={setitems}
-          ></DiscountModal>
-          <SideMenu></SideMenu>
-          <Flex w={"90vw"} h={"100vh"}>
-            <Flex flexDir={"column"} w={"75%"} overflowY={"auto"}>
-              <Table fontSize={"lg"} w={"100%"}>
-                <Thead>
-                  <Tr>
-                    <Th>Nome</Th>
-                    <Th>Valor</Th>
-                    <Th></Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {items &&
-                    items.map((item, index) => (
-                      <Tr key={index}>
-                        <Td>{item.name}</Td>
-                        <Td>{item.price}</Td>
-                        <Td>
-                          <Button
-                            colorScheme="red"
-                            size={"sm"}
-                            onClick={(e) => {
-                              removeItem(index);
-                            }}
-                          >
-                            <Icon as={ImCross}></Icon>
-                          </Button>
-                        </Td>
+        <Hotkeys keyName="f3" onKeyDown={onOpenChangeModal}>
+          <Hotkeys keyName="f4" onKeyDown={onOpenNotaFiscalModal}>
+            <Flex w={"100vw"} h={"100vh"} flexDir={"row"}>
+              <NewProductModal
+                isOpen={isNewProductModalOpen}
+                onClose={onCloseNewProductModal}
+              ></NewProductModal>
+              <DiscountModal
+                isOpen={isDiscountModalOpen}
+                onClose={onCloseDiscountModal}
+                total={total}
+                setItems={setitems}
+              ></DiscountModal>
+              <ChangeModal
+                isOpen={isChangeModalOpen}
+                onClose={onCloseChangeModal}
+                setAmountPayed={setamountPayed}
+              ></ChangeModal>
+              <NotaFiscalModal
+                isOpen={isNotaFiscalModalOpen}
+                onClose={onCloseNotaFiscalModal}
+              ></NotaFiscalModal>
+              <SideMenu></SideMenu>
+              <Flex w={"90vw"} h={"100vh"}>
+                <Flex flexDir={"column"} w={"75%"} overflowY={"auto"}>
+                  <Table fontSize={"lg"} w={"100%"}>
+                    <Thead>
+                      <Tr>
+                        <Th>Nome</Th>
+                        <Th>Valor</Th>
+                        <Th></Th>
                       </Tr>
-                    ))}
-                  <Tr ref={bottomRef}></Tr>
-                </Tbody>
-              </Table>
-            </Flex>
-            <Flex flexDir={"column"} h={"100vh"}>
-              <Flex w={"100%"} flexDir={"column"} p="5">
-                <Heading size="md">Ações</Heading>
-                <Label
-                  text={"Inserir desconto (F2)"}
-                  color={"yellow.500"}
-                  icon={FaPercent}
-                ></Label>
-                <Label
-                  text={"Calcular troco (F3)"}
-                  color={"blue.500"}
-                  icon={GiReceiveMoney}
-                ></Label>
-                <Label
-                  text={" Emitir Cupom Fiscal (F4)"}
-                  color={"orange.500"}
-                  icon={FaFileInvoice}
-                ></Label>
-              </Flex>
-              <Flex flexDir={"row"} p="5" justifyContent="space-evenly">
-                <Stack>
-                  <Heading size="md">Total</Heading>
-                  <Heading alignSelf="flex-start" mt={2}>
-                    R$ {total}
-                  </Heading>
-                </Stack>
-              </Flex>
-              <Spacer></Spacer>
-              <Flex flexDir={"column"} p={5}>
-                <Heading size="md">Finalizar venda</Heading>
-                <Flex flexDir={"row"} justifyContent="space-around" mt={5}>
-                  <Label
-                    mx={1}
-                    text={"Débito (F5)"}
-                    color={"facebook.500"}
-                    icon={BsFillCreditCard2FrontFill}
-                    headingSize="xs"
-                  ></Label>
-                  <Label
-                    mx={1}
-                    text={"Crédito (F6)"}
-                    color={"linkedin.500"}
-                    icon={HiCreditCard}
-                    headingSize="xs"
-                  ></Label>
-                  <Label
-                    mx={1}
-                    text={"Dinheiro (F7)"}
-                    color={"green.500"}
-                    icon={FaMoneyBillWave}
-                    headingSize="xs"
-                  ></Label>
+                    </Thead>
+                    <Tbody>
+                      {items &&
+                        items.map((item, index) => (
+                          <Tr key={index}>
+                            <Td>{item.name}</Td>
+                            <Td>{item.price}</Td>
+                            <Td>
+                              <Button
+                                colorScheme="red"
+                                size={"sm"}
+                                onClick={(e) => {
+                                  removeItem(index);
+                                }}
+                              >
+                                <Icon as={ImCross}></Icon>
+                              </Button>
+                            </Td>
+                          </Tr>
+                        ))}
+                      <Tr ref={bottomRef}></Tr>
+                    </Tbody>
+                  </Table>
                 </Flex>
+                <Flex flexDir={"column"} h={"100vh"}>
+                  <Flex w={"100%"} flexDir={"column"} p="5">
+                    <Heading size="md">Ações</Heading>
+                    <Label
+                      text={"Inserir desconto (F2)"}
+                      color={"yellow.500"}
+                      icon={FaPercent}
+                    ></Label>
+                    <Label
+                      text={"Calcular troco (F3)"}
+                      color={"blue.500"}
+                      icon={GiReceiveMoney}
+                    ></Label>
+                    <Label
+                      text={" Emitir Cupom Fiscal (F4)"}
+                      color={"orange.500"}
+                      icon={FaFileInvoice}
+                    ></Label>
+                  </Flex>
+                  <Flex flexDir={"row"} p="5" justifyContent="space-evenly">
+                    <Stack>
+                      <Heading size="md">Total</Heading>
+                      <Heading alignSelf="flex-start" mt={2}>
+                        R$ {total}
+                      </Heading>
+                      {amountPayed > 0 && (
+                        <>
+                          <Heading size="md">Valor pago</Heading>
+                          <Heading alignSelf="flex-start" mt={2}>
+                            R$ {amountPayed}
+                          </Heading>
+                        </>
+                      )}
+                      {amountPayed > 0 && (
+                        <>
+                          <Heading size="md" color="red">
+                            Troco
+                          </Heading>
+                          <Heading alignSelf="flex-start" mt={2} color="red">
+                            R$ {amountPayed - total}
+                          </Heading>
+                        </>
+                      )}
+                    </Stack>
+                  </Flex>
+                  <Spacer></Spacer>
+                  <Flex flexDir={"column"} p={5}>
+                    <Heading size="md">Finalizar venda</Heading>
+                    <Flex flexDir={"row"} justifyContent="space-around" mt={5}>
+                      <Label
+                        mx={1}
+                        text={"Débito (F5)"}
+                        color={"facebook.500"}
+                        icon={BsFillCreditCard2FrontFill}
+                        headingSize="xs"
+                      ></Label>
+                      <Label
+                        mx={1}
+                        text={"Crédito (F6)"}
+                        color={"linkedin.500"}
+                        icon={HiCreditCard}
+                        headingSize="xs"
+                      ></Label>
+                      <Label
+                        mx={1}
+                        text={"Dinheiro (F7)"}
+                        color={"green.500"}
+                        icon={FaMoneyBillWave}
+                        headingSize="xs"
+                      ></Label>
+                    </Flex>
 
-                <Label
-                  mx={1}
-                  text={"Cancelar venda (F9)"}
-                  color={"red.500"}
-                  icon={MdCancel}
-                  headingSize="xs"
-                ></Label>
+                    <Label
+                      mx={1}
+                      text={"Cancelar venda (F9)"}
+                      color={"red.500"}
+                      icon={MdCancel}
+                      headingSize="xs"
+                    ></Label>
+                  </Flex>
+                </Flex>
               </Flex>
             </Flex>
-          </Flex>
-        </Flex>
+          </Hotkeys>
+        </Hotkeys>
       </Hotkeys>
     </Hotkeys>
   );
